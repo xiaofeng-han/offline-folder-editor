4a22205035264f8a743332176437b34b0ce6cf7e
Xiaofeng Han <hhhanxf@gmail.com>
2019-07-22 13:55:44 -0700

    add main class

---
+++ src/main/java/io/xiaofeng/offlinefoldereditor/Editor.java
@@ -0,0 +1,7 @@
+package io.xiaofeng.offlinefoldereditor;
+
+public class Editor {
+    public static void main(String[] args) {
+        System.out.println("in editor");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module offline.folder.editor {
+    exports io.xiaofeng.offlinefoldereditor;
+}

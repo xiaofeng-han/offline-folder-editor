545eab2012f05a1bda558365141fef167ac99aa2
Xiaofeng Han <hhhanxf@gmail.com>
2020-01-04 13:12:36 -0800

    remove cache stuff

--- src/main/java/io/xiaofeng/offlinefoldereditor/scanner/FolderScanner.java
+++ src/main/java/io/xiaofeng/offlinefoldereditor/scanner/FolderScanner.java
@@ -1,19 +1,17 @@
 package io.xiaofeng.offlinefoldereditor.scanner;
 
 import io.xiaofeng.offlinefoldereditor.model.Entry;
+import lombok.SneakyThrows;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FolderScanner {
+    @SneakyThrows
     public Entry scan(Path startPath) {
         Entry root = Entry.entryBuilder().current(startPath).build();
-        try {
-            Files.walkFileTree(startPath, ScanVisitor.builder().root(root).build());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Files.walkFileTree(startPath, ScanVisitor.builder().root(root).build());
         return root;
     }
 }
